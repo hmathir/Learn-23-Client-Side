11be8d3adc37c1d8a34af0cc56d7f9d9109f9cc0
hmathir <hmathir@gmail.com>
2022-10-25 00:00:54 +0600

    Navbar Responsive

--- src/shared/Header.js
+++ src/shared/Header.js
@@ -6,15 +6,31 @@ import logo from '../assets/logo.png';
 
 const Header = () => {
     const [toggle, setToggle] = useState(false);
-    
+
     return (
-        <div className="flex justify-between w-10/12 mx-auto py-3">
+        <div className="flex justify-between w-11/12 md:w-10/12 mx-auto py-3">
+            <div className="md:hidden dropdown">
+                <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                </label>
+                <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                    <Link>HOME</Link>
+                    <Link>COURSES</Link>
+                    <Link>FAQ</Link>
+                    <Link>BLOGS</Link>
+                    <div className="md:hidden  cursor-pointer" onClick={() => { setToggle(!toggle) }}>
+                        {
+                            toggle ? <div><p>Dark <FontAwesomeIcon icon={faMoon} /></p></div> : <p>Light <FontAwesomeIcon icon={faSun} /></p>
+                        }
+                    </div>
+                </ul>
+            </div>
             <div className="flex items-center gap-6">
-                <div className="flex items-center border border-black rounded-xl px-2">
-                    <h1 className="text-2xl font-bold">LEARN</h1>
-                    <img className="w-12" src={logo} alt="" />
+                <div className="flex items-center border border-black rounded-xl px-2 bg-white">
+                    <h1 className="md:text-2xl font-bold">LEARN</h1>
+                    <img className="w-6 md:w-12" src={logo} alt="" />
                 </div>
-                <div>
+                <div className="hidden md:block">
                     <ul className="space-x-6">
                         <Link>HOME</Link>
                         <Link>COURSES</Link>
@@ -23,10 +39,10 @@ const Header = () => {
                     </ul>
                 </div>
             </div>
-            <div className="flex items-center space-x-6">
-                <div className="cursor-pointer" onClick={()=>{setToggle(!toggle)}}>
+            <div className="flex items-center">
+                <div className="hidden md:block mr-4 cursor-pointer" onClick={() => { setToggle(!toggle) }}>
                     {
-                        toggle ? <div><p>Dark <FontAwesomeIcon icon={faMoon} /></p></div>  : <p>Light <FontAwesomeIcon icon={faSun} /></p>
+                        toggle ? <div><p>Dark <FontAwesomeIcon icon={faMoon} /></p></div> : <p>Light <FontAwesomeIcon icon={faSun} /></p>
                     }
                 </div>
                 <div>
